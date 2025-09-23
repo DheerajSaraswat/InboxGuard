392a58e19c2bf9840f09110d22f82826372c9fb6
Dheeraj Saraswat <dheeraj200410@gmail.com>
2025-09-24 00:37:50 +0530

    AI setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import HomePage from "./pages/HomePage";
-import SigninPage from "./pages/Authentication/SigninPage";
-import SignupPage from "./pages/Authentication/SignupPage";
+import HomePage from "./pages/HomePage.jsx";
+import SigninPage from "./pages/Authentication/SigninPage.jsx";
+import SignupPage from "./pages/Authentication/SignupPage.jsx";
 import { Routes, Route } from "react-router-dom";
-import ForgotPasswordPage from "./pages/Authentication/ForgotPasswordPage";
-import Dashboard from "./pages/dashboard";
+import ForgotPasswordPage from "./pages/Authentication/ForgotPasswordPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import AnimationWrapper from "./common/AnimationWrapper.jsx";
 import VerifyEmailPage from "./pages/Authentication/VerifyEmailPage.jsx";
 import { useSelector } from "react-redux";
@@ -13,21 +13,33 @@ function App() {
   const isDark = theme === "dark";
 
   return (
-    <div className={isDark ? 'dark' : ''}>
+    <div className={isDark ? "dark" : ""}>
       <Routes>
         <Route path="/" element={<HomePage isDark={isDark} />} />
-        <Route path="/signin" element={
-          <AnimationWrapper>
-            <SigninPage isDark={isDark} />
-          </AnimationWrapper>
-        } />
-        <Route path="/signup" element={
-          <AnimationWrapper>
-            <SignupPage isDark={isDark} />
-          </AnimationWrapper>
-        } />
-        <Route path="/verify-email" element={<VerifyEmailPage isDark={isDark} />} />
-        <Route path="/reset-password" element={<ForgotPasswordPage isDark={isDark} />} />
+        <Route
+          path="/signin"
+          element={
+            <AnimationWrapper>
+              <SigninPage isDark={isDark} />
+            </AnimationWrapper>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AnimationWrapper>
+              <SignupPage isDark={isDark} />
+            </AnimationWrapper>
+          }
+        />
+        <Route
+          path="/verify-email"
+          element={<VerifyEmailPage isDark={isDark} />}
+        />
+        <Route
+          path="/reset-password"
+          element={<ForgotPasswordPage isDark={isDark} />}
+        />
         <Route path="/dashboard" element={<Dashboard isDark={isDark} />} />
       </Routes>
     </div>
